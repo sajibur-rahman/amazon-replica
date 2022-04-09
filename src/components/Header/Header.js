d849eb837971ca143926071f408106e8862e2680
sajibur-rahman <sajiburr21@gmail.com>
2022-04-09 15:15:13 +0600

    in header and home component conditional rendaring is done

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,23 +1,56 @@
-import { signOut } from 'firebase/auth';
-import React from 'react';
-import { useAuthState } from 'react-firebase-hooks/auth';
-import { NavLink } from 'react-router-dom';
-import auth from '../../firebase';
-import './Header.css';
+import { signOut } from "firebase/auth";
+import React from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { NavLink } from "react-router-dom";
+import auth from "../../firebase";
+import "./Header.css";
 
 const Header = () => {
-    const [user] =  useAuthState(auth)
-    return (
-        <div>
-            <nav>
-                <NavLink to='/'>Home</NavLink>
-                <NavLink to='/products'>products</NavLink>
-                <NavLink to='/dashboard'>dashboard</NavLink>
-                <NavLink to='/orders'>orders</NavLink>
-                { user?  <button onClick={() => signOut(auth)}>sign out</button> : <NavLink to='/login'>login</NavLink>}
-            </nav>
-        </div>
-    );
+  const [user] = useAuthState(auth);
+  return (
+    <div>
+      <nav>
+        <NavLink
+          style={({ isActive }) =>
+            isActive ? { color: "goldenrod" } : { color: "black" }
+          }
+          to="/"
+        >
+          Home
+        </NavLink>
+        <NavLink
+          style={({ isActive }) =>
+            isActive ? { color: "goldenrod" } : { color: "black" }
+          }
+          to="/products"
+        >
+          products
+        </NavLink>
+        <NavLink
+          style={({ isActive }) =>
+            isActive ? { color: "goldenrod" } : { color: "black" }
+          }
+          to="/dashboard"
+        >
+          dashboard
+        </NavLink>
+        <NavLink
+          style={({ isActive }) =>
+            isActive ? { color: "goldenrod" } : { color: "black" }
+          }
+          to="/orders"
+        >
+          orders
+        </NavLink>
+        {user?.displayName && <span>{user.displayName}</span>}
+        {user ? (
+          <button onClick={() => signOut(auth)}>sign out</button>
+        ) : (
+          <NavLink style={({isActive}) => isActive ? {color:'goldenrod'} :{color:'black'} } to="/login">login</NavLink>
+        )}
+      </nav>
+    </div>
+  );
 };
 
 export default Header;
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,11 +1,19 @@
-import React from 'react';
+import React from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
+import auth from "../../firebase";
 
 const Home = () => {
-    return (
-        <div>
-            <h1>this is Home</h1>
-        </div>
-    );
+  const [user] = useAuthState(auth);
+  return (
+    <div>
+      <h1>
+        current user is{" "}
+        <span style={{ color: "blue", textTransform: "uppercase" }}>
+          {user ? user.displayName : "no user login"}
+        </span>{" "}
+      </h1>
+    </div>
+  );
 };
 
 export default Home;
